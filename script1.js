608864c20666579f8aaa8a18b14e5ba4451e7b86
bndckymt <109018952+bndckymt@users.noreply.github.com>
2024-03-04 18:33:11 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+const pressMeButton = document.getElementById('pressMeButton');
+
+pressMeButton.addEventListener('click', () => {
+    // Redirect to the next page with the "I LOVE YOU!" message and a heart button.
+    window.location.href = 'https://codepen.io/Vaughn-Rafael-De-Castro/full/mdvPVMg';
+});
